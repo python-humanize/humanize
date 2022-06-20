6c1d0c7dfc87189374b5b9095bfdc97ed218a702
Daniel Ching <carterbox@users.noreply.github.com>
2022-06-19 20:33:06 -0500

    BUG: Use %d for year translations convert to string for intcomma after
    This patch fixes a bug introduced in 3.14.0, where the format
    string was changed from %d to %s to add separators to the year.
    However, this needs to happen after translation because the
    translator uses the format strings as part of the translation.
    
    Closes #21

--- src/humanize/time.py
+++ src/humanize/time.py
@@ -202,7 +202,7 @@ def naturaldelta(
         else:
             return _ngettext("1 year, %d day", "1 year, %d days", days) % days
 
-    return _ngettext("%s year", "%s years", years) % intcomma(years)
+    return _ngettext("%d year", "%d years", years).replace("%d", "%s") % intcomma(years)
 
 
 def naturaltime(
