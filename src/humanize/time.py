840faac27bd0dae6f5e23ac0b3dd375d328e42a6
Hugo van Kemenade <hugovk@users.noreply.github.com>
2022-07-09 12:52:30 +0300

    Test precisedelta with None to increase coverage

--- src/humanize/time.py
+++ src/humanize/time.py
@@ -433,7 +433,7 @@ def _suppress_lower_units(min_unit: Unit, suppress: typing.Iterable[Unit]) -> se
 
 
 def precisedelta(
-    value: dt.timedelta | int,
+    value: dt.timedelta | int | None,
     minimum_unit: str = "seconds",
     suppress: typing.Iterable[str] = (),
     format: str = "%0.2f",
