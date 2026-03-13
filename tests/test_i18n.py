d2650e19a2ff8495bc9dc794191ec129d48b6f4f
EPoikans <eriks.poikans@gmail.com>
2026-03-13 13:40:09 +0200

    Seperate test case try block

--- tests/test_i18n.py
+++ tests/test_i18n.py
@@ -29,6 +29,10 @@ def test_i18n() -> None:
         assert humanize.ordinal(5) == "5ый"
         assert humanize.precisedelta(one_min_three_seconds) == "1 минута и 7 секунд"
 
+    except FileNotFoundError:
+        pytest.skip("Generate .mo with scripts/generate-translation-binaries.sh")
+
+    try:
         humanize.i18n.activate("lv")
         assert humanize.naturaltime(three_seconds) == "pirms 3 sekundēm"
         assert humanize.ordinal(5) == "5."
