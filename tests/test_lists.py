0d737e378f83c3e4511293e76ed4c0c58a7b371c
Charlie Tonneslan <cst0520@gmail.com>
2026-05-21 07:47:28 -0400

    Return an empty string from natural_list for an empty list
    natural_list([]) fell through to the >=3-items branch, which does
    items[-1], so it raised IndexError on an empty list instead of
    returning "". Guard the empty case up front.
    
    Signed-off-by: Charlie Tonneslan <cst0520@gmail.com>

--- tests/test_lists.py
+++ tests/test_lists.py
@@ -12,6 +12,7 @@ import humanize
         ([["one", "two", "three"]], "one, two and three"),
         ([["one", "two"]], "one and two"),
         ([["one"]], "one"),
+        ([[]], ""),
         ([[""]], ""),
         ([[1, 2, 3]], "1, 2 and 3"),
         ([[1, "two"]], "1 and two"),
