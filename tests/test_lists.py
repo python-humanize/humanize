5821f19e57e94b4767418f5659a2e57cc25fae9e
Arthur Clark Francisco <arthurclarkjj44@gmail.com>
2026-08-25 12:24:19 -0300

    fix(lists): support general iterables and update typing in natural_list

--- tests/test_lists.py
+++ tests/test_lists.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+from collections.abc import Iterable
+from typing import Any
+
 import pytest
 
 import humanize
@@ -16,9 +19,15 @@ import humanize
         ([[""]], ""),
         ([[1, 2, 3]], "1, 2 and 3"),
         ([[1, "two"]], "1 and two"),
+        ([("one", "two", "three")], "one, two and three"),
+        ([("one", "two")], "one and two"),
+        ([("one",)], "one"),
+        ([{"one": 1, "two": 2}.keys()], "one and two"),
+        ([(x for x in ["one", "two", "three"])], "one, two and three"),
+        ([range(1, 4)], "1, 2 and 3"),
     ],
 )
 def test_natural_list(
-    test_args: list[str] | list[int] | list[str | int], expected: str
+    test_args: Iterable[Any], expected: str
 ) -> None:
     assert humanize.natural_list(*test_args) == expected
